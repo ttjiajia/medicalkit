7a39b031908f57398c89b4687f3319a43aa23230
ttjiajiayou <1339000527@qq.com>
2022-12-06 17:09:32 +0800

    init

---
+++ app/src/main/java/com/zjgsu/medicalkit/drugdetails.java
@@ -0,0 +1,14 @@
+package com.zjgsu.medicalkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class drugdetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drugdetails);
+    }
+}
---
+++ app/src/main/java/com/zjgsu/medicalkit/value.java
@@ -0,0 +1,14 @@
+package com.zjgsu.medicalkit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class value extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_value);
+    }
+}
